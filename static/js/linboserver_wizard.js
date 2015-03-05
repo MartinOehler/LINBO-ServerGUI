a6ae20729cae5ca42a215c5397858906cb606e20
Martin Oehler <oehler@knopper.net>
2015-03-05 11:47:24 +0100

    Bugfix: Variable name conflict
    Parameter in ajax.py has been changed from ip to ipaddr.

--- static/js/linboserver_wizard.js
+++ static/js/linboserver_wizard.js
@@ -9,12 +9,12 @@ function createClientFromTemplate() {
 
     var clientgroupselection = document.getElementById('clientgroupselector').value;
     var clienttemplateselection = document.getElementById('templateselector').value;
-    var ip = document.getElementById('newclientip').value;
+    var ipaddr = document.getElementById('newclientip').value;
 
     Dajaxice.linboweb.linboserver.createClientFromTemplate(Dajax.process,
 							   {'clientgroupselection':clientgroupselection,
 							    'clienttemplateselection':clienttemplateselection,
-							    'ip':ip});
+							    'ipaddr':ipaddr});
 
     return(false);
 };
